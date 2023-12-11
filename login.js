aaaa68f6726e2755facb022a8d6a107011754e64
Louise Findlay <louise@louisefindlay.com>
2023-12-11 10:40:35 +0000

    In progress partial state

--- login.js
+++ login.js
@@ -1,24 +1,20 @@
 const loginForm = document.getElementById("ttr-login");
-let appToken = null;
-let authToken = null;
 
 loginForm.addEventListener("submit", () => {
   event.preventDefault();
-  console.info("Form submitted");
+  console.info("Login form submitted");
   const loginFormData = new FormData(loginForm);
   const username = loginFormData.get("username");
   const password = loginFormData.get("password");
   login(username, password);
 });
 
-function login(username, password, appToken, authToken) {
+function login(username, password) {
   fetch("https://www.toontownrewritten.com/api/login?format=json", {
     method: "POST",
     body: {
       username: username,
       password: password,
-      appToken: appToken,
-      authToken: authToken,
     },
     headers: {
       "Content-Type": "x-www-form-urlencode",
@@ -32,22 +28,22 @@ function login(username, password, appToken, authToken) {
       }
     })
     .then(function (data) {
+      console.info(data);
       switch (data.success) {
         case "false":
           console.info("Failure");
+          console.info(`Username: ${username} Password: ${password}`);
           document.getElementById("ttr-login-error").innerHTML = data.banner;
           break;
         case "partial":
           console.info("Partial");
           document.getElementById("ttr-login-error").innerHTML = data.banner;
           const authToken = data.responseToken;
-          let tfaForm = document.getElementById("ttr-tfa");
-          tfaForm = new FormData(tfaForm);
-          const appToken = form.get("tfa-token");
-          login(username, password, authToken, appToken);
+          //tfa(username, password, authToken);
           break;
         case "delayed":
           // code block
+          console.info("Delayed");
           break;
         case true:
           // code block
@@ -59,3 +55,64 @@ function login(username, password, appToken, authToken) {
       console.warn("Something went wrong.", err);
     });
 }
+
+/* function tfa(username, password) {
+  fetch("https://www.toontownrewritten.com/api/login?format=json", {
+    method: "POST",
+    body: {
+      username: username,
+      password: password,
+    },
+    headers: {
+      "Content-Type": "x-www-form-urlencode",
+    },
+  })
+    .then(function (response) {
+      if (response.ok) {
+        console.info(response.json());
+        return response.json();
+      } else {
+        return Promise.reject(response);
+      }
+    })
+    .then(function (data) {
+      console.log(data);
+      switch (data.success) {
+        case "false":
+          console.info("Failure");
+          document.getElementById("ttr-login-error").innerHTML = data.banner;
+          break;
+        case "partial":
+          console.info("Partial");
+          document.getElementById("ttr-login-error").innerHTML = data.banner;
+          const authToken = data.responseToken;
+          break;
+        case "delayed":
+          // code block
+          console.info("Delayed");
+          break;
+        case true:
+          // code block
+          console.info("Success");
+          break;
+        default:
+          console.info(data);
+      }
+    })
+    .catch(function (err) {
+      console.warn("Something went wrong.", err);
+    });
+}
+
+const tfaForm = document.getElementById("ttr-tfa");
+
+tfaForm.addEventListener("submit", () => {
+  event.preventDefault();
+  console.info("Tfa form submitted");
+  const tfaFormData = new FormData(tfaForm);
+  const appToken = form.get("tfa-token");
+  console.info(username, password, authToken, appToken);
+  tfa(username, password, authToken, appToken);
+});
+
+function tfa() {} */
